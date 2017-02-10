71dfac39eb89f975add8372ec24b432d8a8189cd
jingal <ilovejinjoo@gmail.com>
2017-02-11 01:09:27 +0900

    Server Implemented

---
+++ UltraSonicServer.js
@@ -0,0 +1,66 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var wpi = require('wiring-pi');
+
+var led_pin = 25;
+var btn_pin = 29;
+var sensor_pin = 26;
+
+wpi.setup('wpi');
+
+wpi.pinMode(led_pin, wpi.OUTPUT);
+wpi.pinMode(btn_pin, wpi.INPUT);
+wpi.pinMode(sensor_pin, wpi.INPUT);
+
+var value = 0.1;
+
+app.get('/', function(req, res){
+	//res.send('<h1>Hello world</h1>');
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/on.html', function(req, res){
+	//res.send('<h1>Hello world</h1>');	
+	//res.send('Hello /');
+	wpi.digitalWrite(led_pin, 1);
+});
+
+app.get('/off.html', function(req, res){
+	//res.send('<h1>Hello world</h1>');	
+	//res.send('Hello /');
+	wpi.digitalWrite(led_pin, 0);
+});
+
+
+setInterval(function() {
+	
+	var input = wpi.digitalRead(btn_pin);
+	
+	if(input) {
+		console.log('input data' + input);
+		io.emit('sensor message', value);
+		value += 0.1;
+	}
+	
+}, 500);
+
+
+var value = 0.0;
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+	
+});
+
+http.listen(3000, function() {
+	console.log('listening on *:3000');
+});
